c591df52ba5fecac312bcaede72fa1dffa432818
Seg <seg@segonmedia.com>
2013-03-03 21:15:18 -0800

    Initial Check-in

---
+++ index.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns:fb='http://www.facebook.com/2008/fbml' xmlns:og='http://opengraphprotocol.org/schema/' xmlns='http://www.w3.org/1999/xhtml'>
+<head>
+<title>Understanding Video Games</title>
+<link href='/favicon.ico' rel='shortcut icon' type='image/x-icon' />
+<link href='http://www.understanding-videogames.com' rel='index' title='Understanding Video Games' />
+<meta content="summary" name="twitter:card" />
+<meta content="13000017" property="fb:admins" />
+<meta content="Understanding Video Games" property="og:title" />
+<meta content="http://www.understanding-videogames.com/" property="og:url" />
+<meta content="Understanding Video Games by Space Between Studios." property="og:description" />
+<meta content="website" property="og:type" />
+<meta content="Understanding Video Games" property="og:site_name" />
+<meta content="http://www.understanding-videogames.com/assets/uvg_logo_512.png" property="og:image" />
+
+<link href="/assets/application.css" media="screen" rel="stylesheet" type="text/css" />
+<!--<script src="/assets/application.js" type="text/javascript"></script>-->
+<script type='text/javascript'>
+  //<![CDATA[
+    var _gaq = _gaq || [];
+    var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+    _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+    _gaq.push(['_setAccount', 'UA-37064735-1']);
+    _gaq.push(['_setDomainName', 'understanding-videogames.com']);
+    _gaq.push(['_setAllowLinker', true]);
+    _gaq.push(['_trackPageview']);
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  //]]>
+</script>
+</head>
+
+<body>
+<div id='fb-root'></div>
+<div id='maincontent'>
+<img alt="Understanding Video Games" src="/assets/uvg_web_mainlogo.png" />
+<p>&copy; <?php echo date("Y"); ?> <a href="http://www.spacebetweenstudios.com">Space Between Studios</a></p>
+</div>
+
+</body>
+</html>
